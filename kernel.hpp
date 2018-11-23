6af28fd3340d1d0ad5003fc9e1787c24123d8cee
Jonathan Zrake <jonathan.zrake@gmail.com>
2018-11-23 17:53:19 -0500

    Add tests and start kernel methods

--- kernel.hpp
+++ kernel.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include <set>
+#include <unordered_set>
 #include "any.hpp"
 
 
@@ -12,7 +12,9 @@
 namespace crt
 {
     class expression;
+    class kernel;
     class parser;
+
     enum class data_type { none, i32, f64, str, symbol, composite };
     using list_t = std::vector<linb::any>;
     using dict_t = std::unordered_map<std::string, linb::any>;
@@ -29,10 +31,17 @@ public:
     struct none {};
 
     expression()                                        : type(data_type::none) {}
+    expression(const none&)                             : type(data_type::none) {}
     expression(int vali32)                              : type(data_type::i32), vali32(vali32) {}
     expression(double valf64)                           : type(data_type::f64), valf64(valf64) {}
     expression(const std::string& valstr)               : type(data_type::str), valstr(valstr) {}
-    expression(std::initializer_list<expression> parts) : type(data_type::composite), parts(parts) {}
+    expression(std::initializer_list<expression> parts) : type(data_type::composite), parts(parts)
+    {
+        if (expression::parts.empty())
+        {
+            type = data_type::none;
+        }
+    }
 
     static expression symbol(const std::string& v)
     {
@@ -45,7 +54,7 @@ public:
     static expression composite(const std::vector<expression>& v)
     {
         auto e = expression();
-        e.type = data_type::composite;
+        e.type = v.empty() ? data_type::none : data_type::composite;
         e.parts = v;
         return e;
     }
@@ -95,6 +104,11 @@ public:
         return dict_parts;
     }
 
+    const expression& at(std::size_t index) const
+    {
+        return parts.at(index);
+    }
+
     std::size_t size() const
     {
         return parts.size();
@@ -102,7 +116,35 @@ public:
 
     bool empty() const
     {
-        return type == data_type::none;
+        return
+        (type == data_type::none) ||
+        (type == data_type::composite && parts.empty());
+    }
+
+    double get_i32() const
+    {
+        switch(type)
+        {
+            case data_type::none     : return 0.0;
+            case data_type::i32      : return vali32;
+            case data_type::f64      : return valf64;
+            case data_type::str      : return std::atoi(valstr.data());
+            case data_type::symbol   : return 0.0;
+            case data_type::composite: return 0.0;
+        }
+    }
+
+    double get_f64() const
+    {
+        switch(type)
+        {
+            case data_type::none     : return 0.0;
+            case data_type::i32      : return vali32;
+            case data_type::f64      : return valf64;
+            case data_type::str      : return std::atof(valstr.data());
+            case data_type::symbol   : return 0.0;
+            case data_type::composite: return 0.0;
+        }
     }
 
     std::string str() const
@@ -129,7 +171,7 @@ public:
         }
     }
 
-    std::set<std::string> symbols() const
+    std::unordered_set<std::string> symbols() const
     {
         switch (type)
         {
@@ -140,7 +182,7 @@ public:
             case data_type::symbol   : return {valsym};
             case data_type::composite:
             {
-                std::set<std::string> res;
+                std::unordered_set<std::string> res;
 
                 for (const auto& part : parts)
                 {
@@ -184,14 +226,14 @@ public:
 
     bool operator==(const expression& other) const
     {
-        return (true
-        && type    == other.type
-        && vali32  == other.vali32
-        && valf64  == other.valf64
-        && valstr  == other.valstr
-        && valsym  == other.valsym
-        && keyword == other.keyword
-        && parts   == other.parts);
+        return
+        type    == other.type    &&
+        vali32  == other.vali32  &&
+        valf64  == other.valf64  &&
+        valstr  == other.valstr  &&
+        valsym  == other.valsym  &&
+        keyword == other.keyword &&
+        parts   == other.parts;
     }
 
     bool operator!=(const expression& other) const
@@ -210,6 +252,125 @@ private:
     friend class parser;
 };
 
+
+
+
+// ============================================================================
+class crt::kernel
+{
+public:
+    struct node_t;
+    using map_t = std::unordered_map<std::string, node_t>;
+    using set_t = std::unordered_set<std::string>;
+
+    struct node_t
+    {
+        expression expr;
+        linb::any value;
+        std::string key;
+        std::string error;
+        set_t incoming;
+        set_t outgoing;
+    };
+
+    void insert(const std::string& key, const expression& expr)
+    {
+        node_t node;
+        node.expr = expr;
+        nodes.emplace(key, node);
+    }
+
+    void insert(const std::string& key, const linb::any& value)
+    {
+        node_t node;
+        node.value = value;
+        nodes.emplace(key, node);
+    }
+
+    void erase(const std::string& key)
+    {
+        nodes.erase(key);
+        dirty.erase(key);
+    }
+
+    const expression& expr_at(const std::string& key)
+    {
+        return nodes.at(key).expr;
+    }
+
+    const linb::any& at(const std::string& key)
+    {
+        return nodes.at(key).value;
+    }
+
+    std::size_t size() const
+    {
+        return nodes.size();
+    }
+
+    bool contains(const std::string& key) const
+    {
+        return nodes.find(key) != nodes.end();
+    }
+
+    /** Return the incoming edges for the given node. An empty set is returned if
+        the key does not exist in the graph.
+    */
+    set_t incoming(const std::string& key) const
+    {
+        auto node = nodes.find(key);
+
+        if (node == nodes.end())
+        {
+            return {};
+        }
+        return node->second.incoming;
+    }
+
+    /** Return the outgoing edges for the given node. Even if that node does not exist
+        in the graph, it may have outgoing edges if other nodes in the graph name it
+        as a dependency. If the node does exist in the graph, this is a fast operation
+        because outgoing edges are cached and kept up-to-date.
+    */
+    set_t outgoing(const std::string& key) const
+    {
+        auto node = nodes.find(key);
+
+        if (node == nodes.end())
+        {
+            set_t out;
+            /*
+             Search the graph for nodes naming key as a dependency, and add those nodes
+             to the list of outgoing edges.
+             */
+            for (const auto& other : nodes)
+            {
+                const auto& i = other.second.incoming;
+
+                if (std::find(i.begin(), i.end(), key) != i.end())
+                {
+                    out.insert(other.first);
+                }
+            }
+            return out;
+        }
+        return node->second.outgoing;
+    }
+
+    auto begin() const
+    {
+        return nodes.begin();
+    }
+
+    auto end() const
+    {
+        return nodes.end();
+    }
+
+private:
+    map_t nodes;
+    set_t dirty;
+};
 
 
 
@@ -341,7 +502,7 @@ public:
     {
         assert(*c == '(');
         const char* start = c++;
-        auto e = expression::composite({});
+        auto e = expression();
 
         while (*c != ')')
         {
@@ -357,6 +518,10 @@ public:
             {
                 e.parts.push_back(parse_part(c));
             }
+        }
+        if (! e.parts.empty())
+        {
+            e.type = data_type::composite;
         }
         return e;
     }
@@ -423,6 +588,13 @@ TEST_CASE("expression passes basic sanity tests", "[expression]")
     REQUIRE(expression{1, 2} != expression());
     REQUIRE(expression().empty());
     REQUIRE(expression().dtype() == data_type::none);
+    REQUIRE(expression::composite({}).empty());
+    REQUIRE(expression::composite({}).dtype() == data_type::none);
+    REQUIRE(expression() == expression::none());
+    REQUIRE(expression() == expression({}));
+    REQUIRE(expression() == expression{});
+    REQUIRE(expression() == expression::composite({}));
+    REQUIRE(expression() != expression::composite({1, 2, 3}));
 }
 
 
@@ -451,13 +623,25 @@ TEST_CASE("nested expression can be constructed by hand", "[expression]")
 
 
 
-TEST_CASE("parser works", "[parser]")
+TEST_CASE("expression can be converted to string", "[expression]")
+{
+    REQUIRE(expression().str() == "()");
+    REQUIRE(expression({}).str() == "()");
+    REQUIRE(expression({1, 2, 3}).str() == "(1 2 3)");
+    REQUIRE(expression({1, 2, 3}).str() == "(1 2 3)");
+}
+
+
+
+
+TEST_CASE("basic strings can be parsed into expressions", "[parser]")
 {
     REQUIRE(parser::parse("a").dtype() == data_type::symbol);
     REQUIRE(parser::parse("1").dtype() == data_type::i32);
     REQUIRE(parser::parse("1.0").dtype() == data_type::f64);
     REQUIRE(parser::parse("(a b c)").dtype() == data_type::composite);
     REQUIRE(parser::parse("(a b c)").size() == 3);
+    REQUIRE(parser::parse("(a b b c 1 2 'ant')").symbols().size() == 3);
     REQUIRE(parser::parse("(1 2 3)") == expression{1, 2, 3});
     REQUIRE(parser::parse("(1.0 2.0 3.0)") == expression{1.0, 2.0, 3.0});
     REQUIRE(parser::parse("a=1").key() == "a");
@@ -467,4 +651,62 @@ TEST_CASE("parser works", "[parser]")
         std::string("dragon")});
     REQUIRE_THROWS(parser::parse("1.2.0"));
 }
+
+
+
+
+TEST_CASE("numeric constants parse correctly", "[parser]")
+{
+    REQUIRE(parser::parse ("12").get_i32() == 12);
+    REQUIRE(parser::parse ("13").get_i32() == 13);
+    REQUIRE(parser::parse ("+12").get_i32() == 12);
+    REQUIRE(parser::parse ("-12").get_i32() ==-12);
+    REQUIRE(parser::parse ("13.5").get_f64() == 13.5);
+    REQUIRE(parser::parse ("+13.5").get_f64() == 13.5);
+    REQUIRE(parser::parse ("-13.5").get_f64() ==-13.5);
+    REQUIRE(parser::parse ("+13.5e2").get_f64() == 13.5e2);
+    REQUIRE(parser::parse ("-13.5e2").get_f64() ==-13.5e2);
+    REQUIRE(parser::parse ("+13e2").get_f64() == 13e2);
+    REQUIRE(parser::parse ("-13e2").get_f64() ==-13e2);
+    REQUIRE(parser::parse ("-.5").get_f64() == -.5);
+    REQUIRE(parser::parse ("+.5").get_f64() == +.5);
+    REQUIRE(parser::parse (".5").get_f64() == +.5);
+    REQUIRE_THROWS(parser::parse ("-"));
+    REQUIRE_THROWS(parser::parse ("1e2e2"));
+    REQUIRE_THROWS(parser::parse ("1.2.2"));
+    REQUIRE_THROWS(parser::parse ("1e2.2"));
+    REQUIRE_THROWS(parser::parse ("13a"));
+}
+
+
+
+
+TEST_CASE("keyword expressions parse correctly", "[parser]")
+{
+    REQUIRE(parser::parse("a=1").dtype() == data_type::i32);
+    REQUIRE(parser::parse("a=1").key() == "a");
+    REQUIRE(parser::parse("cow='moo'").dtype() == data_type::str);
+    REQUIRE(parser::parse("cow='moo'").key() == "cow");
+    REQUIRE(parser::parse("deer=(0 1 2 3)").dtype() == data_type::composite);
+    REQUIRE(parser::parse("deer=(0 1 2 3)").key() == "deer");
+    REQUIRE(parser::parse("deer=(0 1 2 3)").size() == 4);
+    REQUIRE(parser::parse("deer=(0 1 2 3)").at(0).get_i32() == 0);
+    REQUIRE(parser::parse("deer=(0 1 2 3)").at(1).get_i32() == 1);
+    REQUIRE(parser::parse("deer=(0 1 2 3)").at(2).get_i32() == 2);
+    REQUIRE(parser::parse("deer=(0 1 2 3)").at(3).get_i32() == 3);
+}
+
+
+
+
+TEST_CASE("kernel can be constructed", "[kernel]")
+{
+    kernel k;
+
+    k.insert("a", linb::any(1));
+    k.insert("b", expression(1));
+
+    REQUIRE(k.size() == 2);
+}
+
 #endif
